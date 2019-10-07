7dbebadf55e5a3d24da358a1c02258e7b1c854bc
Vladimir <53224754+Vladimir-Mazur@users.noreply.github.com>
2019-10-07 21:37:42 +0300

    HW3

--- app.js
+++ app.js
@@ -1,127 +1,255 @@
-//Объекты. Задачи.
+//Функции. Задачи.
+// задача 1
 
-let phone = {};
+let result = 1;
+result2 = "(";
+argument = "";
 
-phone.product = "iphone";
-phone.price = "1000";
-phone.currency = "dollar";
-phone.details = {};
-phone.details.model;
-phone.details.color;
-
-console.log(phone);
-
-//Тернарный оператор. Switch case. Задачи.
-//Задача 1
-let a = "block";
-
-switch (a) {
-  case "block":
-    console.log("block");
-    break;
-  case "none":
-    console.log("none");
-    break;
-  case "inline":
-    console.log("inline");
-    break;
-  default:
-    console.log("other");
-    break;
-}
-
-//Задача 2.1
-let overflow = "hidden";
-
-overflow = overflow === "hidden" ? "visible" : "hidden";
-
-console.log(overflow);
-
-//Задача 2.2
-let x = 0;
-
-x = x === 0 ? 1 : x < 0 ? "less then zero" : x * 10;
-
-console.log(x);
-
-//Задача 2.3
-let car = {
-  name: "Lexus",
-  age: 10,
-  create: 2008,
-  needRepair: false
-};
-
-car.age > 5 ? (needRepair = true) : (needRepair = false);
-car.age > 5 ? console.log("Need Repair") : (needRepair = false);
-
-//Циклы. Задачи.
-//Задача 1
-let easycode = "i am in the easycode";
-newEasycode = easycode[0].toUpperCase();
-
-for (let i = 0; i < easycode.length - 1; i++) {
-  if (easycode[i] === " ") {
-    newEasycode += easycode[i + 1].toUpperCase();
+function multiply() {
+  if (arguments.length > 0) {
+    for (let i = 0; i < arguments.length; i++) {
+      result = result * arguments[i];
+      if (i !== 0) {
+        argument += "," + arguments[i];
+      } else {
+        argument += arguments[i];
+      }
+      if (i !== 0) {
+        result2 += "*" + arguments[i];
+      } else {
+        result2 += arguments[i];
+      }
+    }
+    return (
+      "multiply" +
+      "(" +
+      argument +
+      ")" +
+      " " +
+      "=" +
+      " " +
+      result +
+      result2 +
+      ")"
+    );
   } else {
-    newEasycode += easycode[i + 1];
+    result = 0;
+    return result;
   }
 }
 
-console.log(newEasycode);
+console.log(multiply(1, 2, 3));
+
+// задача 2
+let newString = "";
+
+function getString(test) {
+  for (let i = test.length - 1; i >= 0; i--) {
+    newString += test[i];
+  }
+  return (
+    "reverseString" +
+    "('" +
+    test +
+    "')" +
+    " " +
+    "//" +
+    " " +
+    "'" +
+    newString +
+    "'"
+  );
+}
+
+console.log(getString("test"));
+
+// задача 3
+let unicodLetters = "";
+
+function unicodString(string) {
+  for (let i = 0; i < string.length; i++) {
+    if (i !== 0) {
+      unicodLetters += " " + string[i].charCodeAt();
+    } else {
+      unicodLetters += string[i].charCodeAt();
+    }
+  }
+  return (
+    "getCodeStringFromText" +
+    "('" +
+    string +
+    "')" +
+    " " +
+    "//" +
+    " " +
+    "'" +
+    unicodLetters +
+    "'"
+  );
+}
+
+console.log(unicodString("hello"));
+
+// задача 4
+let answer = "";
+
+function matchNumber(number, randomNumber) {
+  if (number <= 10) {
+    if (number >= 0) {
+      randomNumber = Math.random().toFixed(1) * 10;
+      if (number === randomNumber) {
+        answer = "Вы выиграли";
+      } else {
+        answer =
+          "Вы не угадали ваше число " +
+          number +
+          " а выпало число " +
+          randomNumber;
+      }
+    } else {
+      answer = "число должно быть от 1 до 10";
+    }
+  } else {
+    answer = "число должно быть от 1 до 10";
+  }
+  return answer;
+}
+
+console.log(matchNumber(5));
+
+// задача 5
+let nArray = [];
+
+function getArray(n) {
+  for (let i = 1; i <= n; i++) {
+    nArray.push(i);
+  }
+  return "getArray" + "(" + n + ")" + " " + "//" + " " + "[" + nArray + "]";
+}
+
+console.log(getArray(10));
+
+// задача 6
+let secondArray = [];
+
+function doubleArray(firstArray) {
+  for (let i = 0; i < firstArray.length; i++) {
+    secondArray.push(firstArray[i]);
+  }
+  for (let i = 0; i < firstArray.length; i++) {
+    secondArray.push(firstArray[i]);
+  }
+  return (
+    "doubleArray" +
+    "(" +
+    firstArray +
+    ")" +
+    " " +
+    "//" +
+    " " +
+    "[" +
+    secondArray +
+    "]"
+  );
+}
+
+console.log(doubleArray([1, 2, 3]));
+
+// задача 7
+let withoutFirstArray = [];
+let arrays = [];
+
+function changeCollection() {
+  for (let i = 0; i < arguments.length; i++) {
+    let length = arguments[i];
+    arrays += " [" + arguments[i] + "] ";
+    if (i !== 0) {
+      withoutFirstArray += " [" + length.slice(1, length.length) + "]";
+    } else {
+      withoutFirstArray += "[" + length.slice(1, length.length) + "]";
+    }
+  }
+  return (
+    "changeCollection" +
+    "[" +
+    arrays +
+    "]" +
+    " " +
+    "--->" +
+    " " +
+    "[" +
+    withoutFirstArray +
+    "]"
+  );
+}
+
+console.log(changeCollection([1, 2, 3], [1, 3, 5], [1, 4, 7, 5, 6]));
+
+// задача 8
+frontendDeveloper = {
+  name: "Denis",
+  age: "29",
+  gender: "male"
+};
+
+backendDeveloper = {
+  name: "Ivan",
+  age: "20",
+  gender: "male"
+};
+
+let user = [];
+
+function funcGetUsers() {
+  for (let i = 0; i < arguments.length; i++) {
+    if (arguments[i].gender === "male") {
+      user.push(arguments[i]);
+    } else {
+      user.push("It is not a male");
+    }
+  }
+  return user;
+}
+console.log(funcGetUsers(frontendDeveloper, backendDeveloper));
+
+// Задача 1
+let array = [-2, 3, 4, -5, -6, 2, 4, -56];
+positiveArray = [];
+negativeArray = [];
+
+function positiveNegative() {
+  for (let i = 0; i < array.length; i++) {
+    array[i] > 0
+      ? positiveArray.push(array[i])
+      : array[i] < 0
+      ? negativeArray.push(array[i])
+      : null;
+  }
+
+  return positiveArray.length + " " + negativeArray.length;
+}
+
+console.log(positiveNegative(positiveArray, negativeArray));
 
 //Задача 2
-let invertString = "tseb eht ma i";
-newInvertString = "";
+let arrayNumbers = [1, 2, 3, 5, 8, 9, 10];
+result = [];
+boolean = true;
 
-for (let i = invertString.length - 1; i >= 0; i--) {
-  newInvertString += invertString[i];
-}
-
-console.log(newInvertString);
-
-//Задача 3
-let factorial = 10;
-newFactorial = 1;
-
-for (let i = 1; i <= factorial; i++) {
-  newFactorial = newFactorial * i;
-}
-
-console.log(newFactorial);
-
-//Задача 4
-let string = "JavaScript is a pretty good language";
-newString = "";
-
-for (let i = 0; i < string.length; i++) {
-  if (string[i] !== " ") {
-    newString += string[i].toUpperCase();
+for (const element of arrayNumbers) {
+  if (element % 2 === 1) {
+    boolean = true;
+    result.push({
+      digit: element,
+      odd: boolean
+    });
+  } else {
+    boolean = false;
+    result.push({
+      digit: element,
+      odd: boolean
+    });
   }
 }
 
-console.log(newString);
-
-//Задача 5
-let numArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
-
-for (let value of numArray) {
-  if (value % 2 === 1) {
-    console.log(value);
-  }
-}
-
-//Задача 6
-let list = {
-  name: "denis",
-  work: "easycode",
-  age: 29
-};
-
-for (let key in list) {
-  if (typeof list[key] === "string") {
-    list[key] = list[key].toUpperCase();
-  }
-}
-
-console.log(list);
+console.log(result);
